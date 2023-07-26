6f2bbfa8740c36b78eefd18507d7579e4edda53d
sidharth <sidharthvijayakumar7@gmail.com>
2023-07-26 12:28:41 +0530

    made changes

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.75.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+  project = "angelic-hexagon-307814"
+
+  template {
+    spec {
+      containers {
+        name= "hello"
+        ports {
+            container_port=8080
+        }
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        env {
+            name = "developer"
+            value = "sidharth"
+        }
+        resources {
+            limits = {
+                cpu = "2000m"
+                memory = "1Gi"
+            }
+        }
+      }
+      container_concurrency = 200
+      timeout_seconds=450
+      service_account_name= "svc-terraform@angelic-hexagon-307814.iam.gserviceaccount.com"
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
